6616f7086aa0ca9a50c0f01d4c800443347fc039
george Araujo <georgearaujjo@gmail.com>
2024-10-07 22:17:35 -0300

    Aprendendo conceitos sobre sentenca e bloco de codigo

---
+++ Inicio-Programacao/desafio_setenca_bloco.js
@@ -0,0 +1,57 @@
+/*
+    // Bloco 1 - Exibir no console os números ímpares de 1 á 10
+        1
+        3
+        5
+        7
+        9
+
+    // Bloco 2 - Exibir números pares de 1 á 10
+        2
+        4
+        6
+        8
+        10
+
+    */
+
+        {   
+            console.log('Exiba apenas os números ímpares de 1 á 10:');
+            console.log(2-1);
+            console.log(1+2);
+            console.log(15/3);
+            console.log(5+2);
+            console.log(9*1);
+
+        }
+
+        {
+            console.log('Exiba apenas os números pares de 1 á 10:');
+            console.log(1+1);
+            console.log(6-2);
+            console.log(3+3);
+            console.log(4*2);
+            console.log(20/2);
+
+        }
+
+        /* Á resposta do professor foi bem mais simples do que eu pensei kkk
+        ele fez apenas repetir a numeração no console, segue abaixc como ele fez:
+        */
+
+        {
+            console.log("1");
+            console.log("3");
+            console.log("5");
+            console.log("7");
+            console.log("9");
+        }
+
+        {
+            console.log("2");
+            console.log("4");
+            console.log("6");
+            console.log("8");
+            console.log("10");
+        }
+       
